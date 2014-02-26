bd37e496357d9f99d70d09c69e0a6d63d2bce271
Skyler <skymanzanares@hotmail.com>
2014-02-25 18:11:06 -0700

    Added a new case for the switch statement to try and make it a little more readible.

--- MobSpawn/src/com/SkyIsland/MobSpawn/MobSpawn.java
+++ MobSpawn/src/com/SkyIsland/MobSpawn/MobSpawn.java
@@ -2,6 +2,7 @@ package com.SkyIsland.MobSpawn;
 
 import java.lang.reflect.Array;
 
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -28,20 +29,39 @@ public final class MobSpawn extends JavaPlugin {
 		if (cmd.getName().equalsIgnoreCase("spawn_mob")) {
 			//spawn mob with ID
 			if (Array.getLength(args) != 1) {
+				getLogger().info("invalid length on a spawn_mob request");
 				return false;
 			}
+
+			Player player = (Player) sender;
+			Location spawnAt = player.getLocation();
+			spawnAt.add(Math.random()*100, Math.random()*100, Math.random() * 100);
 			
 			//make sure to move from a stupid switch statement
-			switch (Integer.parseInt(args[0])) {
-			case 1:
+			switch (args[0].toLowerCase()) {
+			case "zombie":
 				//just a zombie
-				Player player = (Player) sender;
-				player.getWorld().spawnEntity(player.getLocation(), EntityType.ZOMBIE);
+				player.getWorld().spawnEntity(spawnAt, EntityType.ZOMBIE);
+				return true;
+			case "giant":
+				//giant zombie
+				player.getWorld().spawnEntity(spawnAt, EntityType.GIANT);
+				return true;
+			case "creeper":
+				player.getWorld().spawnEntity(spawnAt, EntityType.CREEPER);
 				return true;
 			default:
+				getLogger().info("defaulted to false; no string matches on spawn_mob request");
 				return false;
 			}
 			
+			
+		}
+		if (cmd.getName().equalsIgnoreCase("spawn_constants")) {
+			//print out constants
+			Player player = (Player) sender;
+			player.chat("zombie | giant | creeper");
+			return true;
 		}
 		
 		return false;
