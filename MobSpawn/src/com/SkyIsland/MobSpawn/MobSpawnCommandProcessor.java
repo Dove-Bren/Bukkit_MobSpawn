818178b5635ee05aecddfce2786d31585f288998
William Fong <trigition@gmail.com>
2014-03-01 18:37:19 -0700

    Added function for general mob spawning

--- MobSpawn/src/com/SkyIsland/MobSpawn/MobSpawnCommandProcessor.java
+++ MobSpawn/src/com/SkyIsland/MobSpawn/MobSpawnCommandProcessor.java
@@ -163,4 +163,5 @@ public class MobSpawnCommandProcessor implements CommandExecutor {
 				return false;
 	}
 	
+	
 }
--- MobSpawn/src/com/SkyIsland/MobSpawn/SimpleMobType.java
+++ MobSpawn/src/com/SkyIsland/MobSpawn/SimpleMobType.java
@@ -10,25 +10,23 @@ public class SimpleMobType {
 	 * @param loc The location of the mob
 	 * @return The generated entity
 	 */
-	public Entity CustomEntity (EntityType Type, Location loc) {
+	public static Entity CustomEntity (EntityType Type, Location loc) {
 		return loc.getWorld().spawnEntity(loc, Type);
 	}
 	/**
 	 * This is a constructor for defining the entities general characteristics
 	 * @param Type The type of mob to be spawned
 	 * @param loc The location of the mob
-	 * @param fireResistance The amount of ticks a mob will remain on fire (lower levels mean higher difficulty)
-	 * @param ticksLived The amount of ticks the mob has lived (higher levels mean higher difficulty)
+	 * @param ticksLived The amount of ticks the mob has 'lived' (higher levels mean higher difficulty)
 	 * @return The generated entity
 	 */
-	public Entity CustomEntity (EntityType Type, Location loc, int fireResistance, int ticksLived) {
-		if (fireResistance < 0 || ticksLived < 0) {
-			System.err.println("ERROR: No negative fireResistance and ticksLived parameters allowed!");
-			System.err.println("Not applying fireResistance and ticksLived parameters");
+	public static Entity CustomEntity (EntityType Type, Location loc, int ticksLived) {
+		if (ticksLived < 0) {
+			System.err.println("ERROR: No negative ticksLived parameter allowed!");
+			System.err.println("Not applying ticksLived parameter");
 			return loc.getWorld().spawnEntity(loc, Type);
 		}
 		Entity newEntity = loc.getWorld().spawnEntity(loc, Type);
-		newEntity.setFireTicks(fireResistance);
 		newEntity.setTicksLived(ticksLived);
 		return newEntity;
 	}
