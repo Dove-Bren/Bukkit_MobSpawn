fb156f1c2ae8f6b83e81f4bd72ad078b143c8b23
William Fong <trigition@gmail.com>
2014-03-01 19:10:09 -0700

    Create spawnEvent.java
    Updated main class to work with both types of spawning process (natural and artificial)

---
+++ MobSpawn/src/com/SkyIsland/MobSpawn/spawnEvent.java
@@ -0,0 +1,59 @@
+package com.SkyIsland.MobSpawn;
+
+import java.util.Random;
+import java.util.Set;
+
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class spawnEvent implements Listener {
+	
+	private MobSpawn plugin;
+	
+	protected spawnEvent(MobSpawn plugin) {
+		this.plugin = plugin;
+	}
+	
+	
+	@EventHandler (priority=EventPriority.HIGH)
+	protected void generateSpawn() {
+		String current = getMob(plugin.mobIdLookup);
+		//CALL WILLIES FUNCTION WITH CURRENT and mobIDLookup.valueof(current)
+	}
+	
+	
+	
+	
+	/**
+	 * 
+	 * @param mobTable the yaml config file to fetch mob from
+	 * @return the keyword for the mob
+	 */
+	public static String getMob(YamlConfiguration mobTable) {
+		Object keys[] = null;
+		Set<String> tempSet = mobTable.getConfigurationSection("Rates").getKeys(true);
+		Random rGen = new Random();
+		
+		keys = tempSet.toArray();
+		
+		int length, pos, chance, actualChance;
+		
+		length = keys.length;
+		
+		while (true) {
+			pos = (int) Math.floor(Math.random() * length); //hopefully 0 through list length-1
+			chance = (int) rGen.nextInt(100);
+			actualChance = mobTable.getInt("Rates." + keys[pos].toString(), 100); //should load up the reported chance, defaulting to 100
+			
+			if (chance <= actualChance) {
+				return keys[pos].toString();
+				
+			}
+		}
+		
+	}
+	
+}
