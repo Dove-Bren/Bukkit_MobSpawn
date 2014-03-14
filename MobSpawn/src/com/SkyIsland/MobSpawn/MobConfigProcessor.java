aaa2501791ed6c037036c19c599bce37611c56d6
m0pt0pmatt <m0pt0pmatt17@gmail.com>
2014-03-13 20:24:10 -0600

    moving parsing code into one class. (one class to parse it all)

--- MobSpawn/src/com/SkyIsland/MobSpawn/MobConfigProcessor.java
+++ MobSpawn/src/com/SkyIsland/MobSpawn/MobConfigProcessor.java
@@ -4,14 +4,18 @@ import java.util.StringTokenizer;
 import java.util.logging.Logger;
 
 import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.inventory.ItemStack;
 
 public final class MobConfigProcessor {
 	
 	final static Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 	private static Entity helper;
+	
 	/**
 	 * This method determines how a new entity(ies) will be generated
 	 * @param Name The name of the entity
@@ -29,9 +33,7 @@ public final class MobConfigProcessor {
 			}
 		}
 		if (tokenString.trim().compareTo("predefined") == 0) {
-			PredefinedMobType.spawnLocalized(PredefinedMobType.valueOf(Name), Loc);
-			
-			
+			PredefinedMobType.spawnLocalized(PredefinedMobType.valueOf(Name), Loc);	
 		}
 		if (tokenString.trim().compareTo("double") == 0) {
 			//Determine how complex types will be generated
@@ -55,7 +57,166 @@ public final class MobConfigProcessor {
 			}
 		}
 		if (tokenString.trim().compareToIgnoreCase("complex") == 0) {
-			complexParser.createFromDefinition(plugin, Name, Loc);
+			createFromDefinition(plugin, Name, Loc);
+		}
+	}
+	
+	
+	//type stores the name of the mob as a string
+	public static void createFromDefinition(MobSpawn plugin, String mobType, Location loc) {
+		ConfigurationSection memSection = plugin.mobIdLookup.getConfigurationSection("Definitions." + mobType);
+		String buffer;
+		
+		int numberOfMobs = memSection.getInt("numberOfPieces", 1), i;
+		LivingEntity mob = null, mob2 = null;
+		boolean isBoss = memSection.getBoolean("isBoss", false);
+		
+		for (i = 1; i <= numberOfMobs && memSection.contains("entity" + i); i++) {
+			
+			mob2 = mob;
+			mob =  (LivingEntity) loc.getWorld().spawnEntity(loc, EntityType.valueOf(memSection.getString("entity" + i)));
+			
+			if (i > 1) {
+				mob2.setPassenger(mob);
+			}
+			
+				//if it's a boss, we don't want it to despawn
+			mob.setRemoveWhenFarAway(!isBoss);
+			
+			//set it's name, if it's not 'none'
+			buffer = memSection.getString("entity" + i + "Name", "none");
+			if (!buffer.trim().equalsIgnoreCase("none")) {
+				mob.setCustomName(buffer);
+			}
+			
+			//get how much hp it has. if for whatever reason we can't get it, we assume 6
+			mob.setMaxHealth(memSection.getDouble("entity" + i + "Hp", 6));
+			
+			//load up what potion effect is has, as long as it's not none.
+			buffer = memSection.getString("entity" + i + "PotionEffect", "none");
+			if (buffer.trim().compareToIgnoreCase("none") != 0) {
+				try { 
+				  PredefinedPotionEffect.valueOf(buffer).potionEffect.apply(mob);
+				}
+				catch (IllegalArgumentException e) {
+					plugin.getLogger().info("Invalid type of potion effect on mob type [" + mobType + "]" + System.getProperty("line.separator") + "Potion effect must be one of the effects supplied in PredefinedPotionEffect.java!");
+					mob.remove();
+					return;
+				}
+				catch (NullPointerException e) {
+	
+					plugin.getLogger().info("Invalid type of potion effect on mob type [" + mobType + "]" + System.getProperty("line.separator") + "Parser is reading a null!");
+					mob.remove();
+					return;
+				}
+			}
+			
+			//all we need to do now is the equipment
+			buffer = memSection.getString("entity" + i + "Equips", "none none none none none");
+			if (!buffer.trim().equalsIgnoreCase("none none none none none")) {
+				//heads chest legs boots inHand
+				StringTokenizer parser = new StringTokenizer(buffer, " ");
+				String miniBuffer;
+				
+				//first part of the string is what equip goes on the head
+				miniBuffer = parser.nextToken();
+				if (!miniBuffer.trim().equalsIgnoreCase("none")) {
+					try { 
+						mob.getEquipment().setHelmet(new ItemStack(Material.valueOf(miniBuffer)));
+					}
+					catch (IllegalArgumentException e) {
+						plugin.getLogger().info("Invalid type of material on mob type [" + mobType + "]" + System.getProperty("line.separator") + "Material must be a valid value of the Material Enumerated type");
+						mob.remove();
+						return;
+					}
+					catch (NullPointerException e) {
+
+						plugin.getLogger().info("Invalid type of material on mob type [" + mobType + "]" + System.getProperty("line.separator") + "Parser is reading a null!");
+						mob.remove();
+						return;
+					}
+				}
+				
+				//next is the chestplate
+				//first part of the string is what equip goes on the head
+				miniBuffer = parser.nextToken();
+				if (!miniBuffer.trim().equalsIgnoreCase("none")) {
+					try { 
+						mob.getEquipment().setChestplate(new ItemStack(Material.valueOf(miniBuffer)));
+					}
+					catch (IllegalArgumentException e) {
+						plugin.getLogger().info("Invalid type of material on mob type [" + mobType + "]" + System.getProperty("line.separator") + "Material must be a valid value of the Material Enumerated type");
+						mob.remove();
+						return;
+					}
+					catch (NullPointerException e) {
+
+						plugin.getLogger().info("Invalid type of material on mob type [" + mobType + "]" + System.getProperty("line.separator") + "Parser is reading a null!");
+						mob.remove();
+						return;
+					}
+				}
+				
+				//next part is the leggings
+				//first part of the string is what equip goes on the head
+				miniBuffer = parser.nextToken();
+				if (!miniBuffer.trim().equalsIgnoreCase("none")) {
+					try { 
+						mob.getEquipment().setLeggings(new ItemStack(Material.valueOf(miniBuffer)));
+					}
+					catch (IllegalArgumentException e) {
+						plugin.getLogger().info("Invalid type of material on mob type [" + mobType + "]" + System.getProperty("line.separator") + "Material must be a valid value of the Material Enumerated type");
+						mob.remove();
+						return;
+					}
+					catch (NullPointerException e) {
+
+						plugin.getLogger().info("Invalid type of material on mob type [" + mobType + "]" + System.getProperty("line.separator") + "Parser is reading a null!");
+						mob.remove();
+						return;
+					}
+				}
+				
+				//fourth part is boots
+				//first part of the string is what equip goes on the head
+				miniBuffer = parser.nextToken();
+				if (!miniBuffer.trim().equalsIgnoreCase("none")) {
+					try { 
+						mob.getEquipment().setBoots(new ItemStack(Material.valueOf(miniBuffer)));
+					}
+					catch (IllegalArgumentException e) {
+						plugin.getLogger().info("Invalid type of material on mob type [" + mobType + "]" + System.getProperty("line.separator") + "Material must be a valid value of the Material Enumerated type");
+						mob.remove();
+						return;
+					}
+					catch (NullPointerException e) {
+
+						plugin.getLogger().info("Invalid type of material on mob type [" + mobType + "]" + System.getProperty("line.separator") + "Parser is reading a null!");
+						mob.remove();
+						return;
+					}
+				}	
+				
+				//last is what's in hand
+				//first part of the string is what equip goes on the head
+				miniBuffer = parser.nextToken();
+				if (!miniBuffer.trim().equalsIgnoreCase("none")) {
+					try { 
+						mob.getEquipment().setItemInHand(new ItemStack(Material.valueOf(miniBuffer)));
+					}
+					catch (IllegalArgumentException e) {
+						plugin.getLogger().info("Invalid type of material on mob type [" + mobType + "]" + System.getProperty("line.separator") + "Material must be a valid value of the Material Enumerated type");
+						mob.remove();
+						return;
+					}
+					catch (NullPointerException e) {
+
+						plugin.getLogger().info("Invalid type of material on mob type [" + mobType + "]" + System.getProperty("line.separator") + "Parser is reading a null!");
+						mob.remove();
+						return;
+					}
+				}
+			}			
 		}
 	}
 }
